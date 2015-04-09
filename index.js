420a385e0fb881d066618b9e9686887de2533443
winniehell <git@winniehell.de>
2015-04-09 18:49:57 +0200

    Move extended app for development to separate file

--- index.js
+++ index.js
@@ -3,24 +3,6 @@
 (function() {
   'use strict';
 
-  var dummyText = require('lorem-ipsum');
-  var app = require('./app');
-
-  app.get('/', function(req, res, next) {
-    res.render('layout.html', {
-      title: 'dummy page',
-      content: dummyText({count: 10}),
-    });
-  });
-
-  app.get('/error', function(req, res, next) {
-    var err = new Error('I’m a teapot');
-    err.status = 418;
-
-    res.render('error.html', {
-      error: err,
-    });
-  });
-
+  var app = require('./app-dev');
   app.listen(parseInt(process.env.PORT) || 62003);
 })();
