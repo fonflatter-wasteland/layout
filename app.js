4e8f4d1478cf22154b4188ea3c58805550241513
winniehell <git@winniehell.de>
2015-04-11 21:53:51 +0200

    Move request handlers to separate functions

--- app.js
+++ app.js
@@ -18,40 +18,48 @@ module.exports = (function() {
   var viewEnv = new nunjucks.Environment(viewLoader, {autoescape: true});
   viewEnv.express(app);
 
-  app.use('/css', express.static(path.join(__dirname, 'css')));
-  app.use('/img', express.static(path.join(__dirname, 'img')));
+  var ASSET_DIRECTORIES = ['css',
+    'img',
+  ];
+
+  ASSET_DIRECTORIES.forEach(function(dir) {
+    app.use('/' + dir, express.static(path.join(__dirname, dir)));
+  });
 
   if (app.get('env') !== 'production') {
+    app.get('/dummy', dummyResponse);
+    app.get('/error', dummyError);
+    app.get('/teapot-error', teapotError);
+  }
+
+  app.use(fallbackHandler);
+  app.use(errorHandler);
+
+  /**
+   * Request handler throwing an error for testing.
+   * @see teapotError
+   */
+  function dummyError(req, res, next) {
+    var err = new Error('Custom error');
+    next(err);
+  }
+
+  /**
+   * Request handler retuning layout template with dummy text for testing.
+   */
+  function dummyResponse(req, res, next) {
     var dummyText = require('lorem-ipsum');
-
-    app.get('/dummy', function(req, res, next) {
-      res.render('layout.html', {
-        title: 'dummy page',
-        content: dummyText({count: 10}),
-      });
-    });
-
-    app.get('/error', function(req, res, next) {
-      var err = new Error('Custom error');
-      next(err);
-    });
-
-    app.get('/teapot-error', function(req, res, next) {
-      var err = new Error('I’m a teapot');
-      err.status = 418;
-      next(err);
+    res.render('layout.html', {
+      title: 'dummy page',
+      content: dummyText({count: 10}),
     });
   }
 
-  // Catch 404 and forward to error handler
-  app.use(function(req, res, _) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    throw err;
-  });
-
-  // Error handler
-  app.use(function(err, req, res, next) {
+  /**
+   * Handles all occurring errors by displaying error page and dumping stack
+   * trace to log.
+   */
+  function errorHandler(err, req, res, next) {
     console.error(err.stack);
 
     if (err.status) {
@@ -63,7 +71,27 @@ module.exports = (function() {
     res.render('error.html', {
       message: err.message,
     });
-  });
+  }
+
+  /**
+   * Handles every request which was not handled already by displaying
+   * an error.
+   */
+  function fallbackHandler(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
+
+  /**
+   * Request handler throwing an error with HTTP status for testing.
+   * @see dummyError
+   */
+  function teapotError(req, res, next) {
+    var err = new Error('I’m a teapot');
+    err.status = 418;
+    next(err);
+  }
 
   return app;
 })();
