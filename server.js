560102c171553b440e0e2dac21dce650f03ea6cc
winniehell <git@winniehell.de>
2015-04-28 02:00:47 +0200

    Simplify server.js

--- server.js
+++ server.js
@@ -1,22 +1,5 @@
 #!/usr/bin/env node
 
-(function() {
-  'use strict';
-
-  var express = require('express');
-  var app = require('./dummy');
-
-  var port = parseInt(process.env.PORT);
-
-  if (port) {
-    console.log('Running at port ' + port + ' ...');
-    app.listen(port);
-  } else {
-    var config = require('./package.json');
-    var hostname = config.name + '.node.js';
-    port = 62000;
-    console.log('Running at http://' + hostname + ':' + port + ' ...');
-    app.listen(port, hostname);
-  }
-
-})();
+var server = require('simple-express-server');
+var app = require('./dummy');
+server(app);
